ad409e2f766223916108e0142b96ab772ee0ea97
Bitbucket Pipelines <khpipelines@khipu.com>
2020-03-09 11:33:37 +0000

    Version

--- src/main/csharp/KhipuChaski/Client/Configuration.cs
+++ src/main/csharp/KhipuChaski/Client/Configuration.cs
@@ -17,7 +17,7 @@ namespace KhipuChaski.Client
         /// Version of the package.
         /// </summary>
         /// <value>Version of the package.</value>
-        public const string Version = "1.1.1";
+        public const string Version = "";
   
         /// <summary>
         /// Gets or sets the default API client for making HTTP calls.
@@ -102,8 +102,8 @@ namespace KhipuChaski.Client
                      .GetExecutingAssembly()
                      .GetReferencedAssemblies()
                      .Where(x => x.Name == "System.Core").First().Version.ToString()  + "\n";
-            report += "    Version of the API: 1.1.1\n";
-            report += "    SDK Package Version: 1.1.1\n";
+            report += "    Version of the API: 1.1.2\n";
+            report += "    SDK Package Version: \n";
   
             return report;
         }
